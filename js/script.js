0a41d6695099ef0cab5f6679732e0531d716836e
yuuumbk <hikoki.y@gmail.com>
2021-04-04 10:35:26 +0900

    topまで完成。ロゴに甘さがあります。

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
 //   $("body").css('margin-top', height);
 // });
 
-$(function(){
-  //ナビゲーションをスライドインさせる
+$(function () {
+  //コンテンツをスライドインさせる
   $(".nav-list").animate({
     right: 0,
   }, 1500, 'easeInOutBack');
@@ -16,4 +16,63 @@ $(function(){
   $(".top .content").animate({
     left: "5%",
   }, 1500, 'easeInOutBack');
+
+  //raindrops
+  $('footer').raindrops({
+    color: '#444',
+    waveLength: 200,
+    frequency: 3,
+    waveHeight: 50,
+    density: 0,
+    rippleSpeed: 0.002,
+    canvasWidth: 1200,
+    canvasHeight: 100,
+    positionBottom: 0,
+    positionLeft: 0
+  });
+
+  //ロゴのアニメーション
+  //twitter
+  $('.logo img[alt="twitter"]').on({
+    'mouseenter': function () {
+      $(this).attr('src', '../img/logo/Logo blue.svg');
+    },
+
+    'mouseleave': function () {
+      $(this).attr('src', '../img/logo/Logo white.svg');
+    }
+  });
+
+  //github
+  $('.logo img[alt="github"]').on({
+    'mouseenter': function () {
+      $(this).attr('src', '../img/logo/github-purple.png');
+    },
+
+    'mouseleave': function () {
+      $(this).attr('src', '../img/logo/github-white.png');
+    }
+  });
+
+  //qiita
+  $('.logo img[alt="qiita"]').on({
+    'mouseenter': function () {
+      $(this).attr('src', '../img/logo/qiita.svg');
+    },
+
+    'mouseleave': function () {
+      $(this).attr('src', '../img/logo/qiita-white.svg');
+    }
+  });
+
+  //email
+  $('.logo img[alt="email"]').on({
+    'mouseenter': function () {
+      $(this).attr('src', '../img/logo/mail-red.svg');
+    },
+
+    'mouseleave': function () {
+      $(this).attr('src', '../img/logo/mail-white.svg');
+    }
+  });
 });
